777e3d7f4da18911deb91426a81fa41b05490fb8
ficoCT <fico.dev.ct@gmail.com>
2022-05-25 20:59:08 +0200

    Fontawesome.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,32 @@
 import './App.css';
 import {Outlet, Link, Route} from "react-router-dom";
+import {Container, Nav, Navbar} from "react-bootstrap";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 function App() {
   return (
       <>
-      <nav>
-      <Link to="/computers-manager">Komputery</Link> |{" "}
-      <Link to="/software">Oprogramowanie</Link> |{" "}
-      <Link to="/otherHardware">Inny sprzęt</Link> |{" "}
-      <Link to="/administration">Administracja</Link> |{" "}
-      <Link to="/timetable">Terminarz</Link> |{" "}
-      <Link to="/reports">Raporty</Link> |{" "}
-      <Link to="/repairs">Naprawy</Link> |{" "}
-      <Link to="/login">Zaloguj się</Link>
-      </nav>
+          <Navbar bg="light" variant="light">
+              <Container>
+                  <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+                  <Nav className="me-auto">
+                      <Link to="/computers-manager">
+                          Komputery
+                          <i className="fa-solid fa-computer"></i>
+                      </Link> |{" "}
+                      <Link to="/software">
+                          Oprogramowanie
+                          <i className="fa-solid fa-display-arrow-down"></i>
+                      </Link> |{" "}
+                      <Link to="/otherHardware">Inny sprzęt</Link> |{" "}
+                      <Link to="/administration">Administracja</Link> |{" "}
+                      <Link to="/timetable">Terminarz</Link> |{" "}
+                      <Link to="/reports">Raporty</Link> |{" "}
+                      <Link to="/repairs">Naprawy</Link> |{" "}
+                      <Link to="/login">Zaloguj się</Link>
+                  </Nav>
+              </Container>
+          </Navbar>
       <Outlet />
       </>
   );
