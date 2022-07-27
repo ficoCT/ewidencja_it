5f6c3a301ef4b0908d0d683c94c0487b8d51e211
ficoCT <fico.dev.ct@gmail.com>
2022-07-27 21:21:57 +0200

    Firebase Manager Admin.

--- src/components/ComputersManagerAdmin/ComputersManagerAdmin.js
+++ src/components/ComputersManagerAdmin/ComputersManagerAdmin.js
@@ -1,20 +1,32 @@
 import React, { useEffect, useState } from 'react';
+import {collection, doc, getDocs, getFirestore, setDoc} from "firebase/firestore";
 import Computer from '../Computer';
 import AddComputer from '../AddComputer';
+import {app} from "../../firebase";
 // import * as API from '../../api/cars';
 
 export default function ComputersManagerAdmin() {
-    const [computers, setComputers] = useState([
-        {id: 1, company:'company A', model:'Desktop 1', inventoryNumber: "1D", serialNumber: "ASX23TD" },
-        {id: 2, company:'company B', model:'Desktop 2', inventoryNumber: "2D", serialNumber: "ASX23TD" },
-        {id: 1, company:'company A', model:'Laptop 1', inventoryNumber: "1L", serialNumber: "ASX23TD" },
-        {id: 2, company:'company B', model:'Laptop 2', inventoryNumber: "2L", serialNumber: "ASX23TD" }
-    ]);
+    // const [computers, setComputers] = useState([
+    //     {id: 1, company:'company A', model:'Desktop 1', inventoryNumber: "1D", serialNumber: "ASX23TD" },
+    //     {id: 2, company:'company B', model:'Desktop 2', inventoryNumber: "2D", serialNumber: "ASX23TD" },
+    //     {id: 1, company:'company A', model:'Laptop 1', inventoryNumber: "1L", serialNumber: "ASX23TD" },
+    //     {id: 2, company:'company B', model:'Laptop 2', inventoryNumber: "2L", serialNumber: "ASX23TD" }
+    // ]);
+    const [computers, setComputers] = useState([]);
+    const db = getFirestore(app);
 
-  // useEffect(() => {
-  //   loadCars();
-  // }, []);
-  //
+    async function getComputers(db) {
+        const computers = collection(db, 'computers');
+        const computersSnapshot = await getDocs(computers);
+        const computersData = computersSnapshot.docs.map(doc => doc.data());
+        return computersData;
+    }
+
+  useEffect(() => {
+      getComputers(db).then(computersData => setComputers(computersData));
+      console.log("computers", computers);
+  }, []);
+
   // function loadCars() {
   //   API.getCars()
   //     .then(setCars);
