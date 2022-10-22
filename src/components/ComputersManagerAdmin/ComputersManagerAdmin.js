305cb5f237b6fa1b2efe9b3d1b36d610bbae3e7a
ficoCT <fico.dev.ct@gmail.com>
2022-10-22 11:25:27 +0200

    Remote company name.

--- src/components/ComputersManagerAdmin/ComputersManagerAdmin.js
+++ src/components/ComputersManagerAdmin/ComputersManagerAdmin.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import {app} from "../../firebase";
 import {
     collection, getFirestore,
     doc, getDocs, addDoc, deleteDoc, updateDoc,
@@ -6,7 +7,6 @@ import {
 } from "firebase/firestore";
 import Computer from '../Computer';
 import AddComputer from '../AddComputer';
-import {app} from "../../firebase";
 import QueryComputer from "../QueryComputer";
 import ToggleVisibility from "../ToggleVisibility";
 
@@ -15,10 +15,7 @@ export default function ComputersManagerAdmin() {
     const [computers, setComputers] = useState([]);
     const [queryComputer, setQueryComputer] = useState([]);
     const [companies, setCompanies] = useState([]);
-    const [company, setCompany] = useState([]);
-    const [models, setModels] = useState([]);
-
-    const [values, setValues] = useState({company: 'dell'});
+    const [values, setValues] = useState({company: 'dell', models: ''});
 
     const db = getFirestore(app);
     const computersRef = collection(db, 'computers');
@@ -27,20 +24,6 @@ export default function ComputersManagerAdmin() {
     const selectItems = {
         name: "size",
         fields: {
-            company: [
-                {
-                    value: "dell",
-                    label: "Dell",
-                },
-                {
-                    value: "asus",
-                    label: "Asus",
-                },
-                {
-                    value: "toshiba",
-                    label: "Toshiba",
-                }
-            ],
             dell: [
                 {
                     value: "modelDell1",
@@ -78,21 +61,27 @@ export default function ComputersManagerAdmin() {
     };
 
     const handleChange = (name, value) => {
-        console.log('[name]: value', name, value);
         setValues((s) => {
             return { ...s, [name]: value };
         });
     };
 
-    async function loadCompany(computersRef) {
+    async function loadCompany(companyRef) {
 
+        let label;
+        let companiesData = [];
         let companyData = [];
-        await getDocs(computersRef).then(snapshot => {
+        await getDocs(companyRef).then(snapshot => {
             snapshot.docs.forEach(doc => {
-                companyData.push({ ...doc.data(), id: doc.id })
+                companyData.push({ ...doc.data(), id: doc.id });
+                label = doc.id.split("");
+                label[0] = label[0].toUpperCase();
+                label.toString();
+                companiesData.push({value: doc.id, label: label});
             })
         })
-        return companyData;
+        console.log('computersData', companiesData);
+        return {companiesData: companiesData, companyData: companyData};
     }
 
     async function loadComputers(computersRef) {
@@ -100,15 +89,19 @@ export default function ComputersManagerAdmin() {
         let computersData = [];
         await getDocs(computersRef).then(snapshot => {
                 snapshot.docs.forEach(doc => {
-                    computersData.push({ ...doc.data(), id: doc.id })
+                    computersData.push({ ...doc.data(), id: doc.id });
                 })
             })
         return computersData;
     }
 
       useEffect(() => {
+
           loadComputers(computersRef).then(computersData => setComputers(computersData));
-          loadCompany(companyRef).then(companyData => setCompanies(companyData));
+          loadCompany(companyRef).then(data => {
+              setCompanies(data.companiesData);
+          });
+
       }, []);
 
       function addComputer(computer) {
@@ -122,6 +115,7 @@ export default function ComputersManagerAdmin() {
               .then(() => {
                   loadComputers(computersRef).then(computersData => setComputers(computersData));
               })
+
       }
 
       function updateComputer(id, computer) {
@@ -140,101 +134,101 @@ export default function ComputersManagerAdmin() {
       }
 
       function deleteComputer(id) {
-          const computerRef = doc(db, 'computers', id)
 
+          const computerRef = doc(db, 'computers', id)
           deleteDoc(computerRef)
               .then(() => {
                   loadComputers(computersRef).then(computersData => setComputers(computersData));
               })
+
       }
 
     function queryComputers(values) {
+
         const q = query(computersRef, where("company", "==", values.company));
         loadComputers(q).then(qC => setQueryComputer(qC));
+
     }
 
   return (
     <>
-      <div>
-          <span style={{color:'red'}}>Lista komputerów w przedsiębiorstwie</span>
-      <ToggleVisibility>
-      <div>
-      {computers.length === 0 ?
-          <h1>Ładowanie danych ...</h1>
-          :
-          <ul>
-            {computers.map(computer => (
-              <li key={computer.id}>
-                <Computer computer={computer} onUpdate={updateComputer} onDelete={deleteComputer} />
-              </li>
-            ))}
-          </ul>
-      }
-      <br />
-      </div>
-          </ToggleVisibility>
-      </div>
-      <div>
-          <span style={{color:'red'}}>DODAJ KOMPUTER</span>
-          <ToggleVisibility>
-              <AddComputer onSubmit={addComputer} />
-          </ToggleVisibility>
 
-      </div>
-      <br />
-      <span style={{color:'red'}}>WYSZUKAJ KOMPUTER</span>
+      <span style={{color:'red'}}>Lista komputerów w przedsiębiorstwie</span>
       <ToggleVisibility>
       <div>
-        <QueryComputer submitLabel="Wyszukaj" onSubmit={queryComputers} />
-        {queryComputer.length === 0 ?
-            ''
-            :
-            <ul>
-                {queryComputer.map(computer => (
-                    <li key={computer.id}>
-                        <Computer computer={computer} onUpdate={updateComputer} onDelete={deleteComputer} />
-                    </li>
+          {computers.length === 0 ?
+              <h1>Ładowanie danych ...</h1>
+              :
+              <ul>
+                {computers.map(computer => (
+                  <li key={computer.id}>
+                    <Computer computer={computer} onUpdate={updateComputer} onDelete={deleteComputer} />
+                  </li>
                 ))}
-            </ul>
-        }
+              </ul>
+          }
       </div>
       </ToggleVisibility>
-        <hr />
-        <div>
-        <span style={{color:'red'}}>Lista modeli komputerów w przedsiębiorstwie</span>
-        <ToggleVisibility>
-        <div>
-        <select
-            id="size1"
-            name="size1"
-            onChange={(e) => handleChange("company", e.target.value)}
-        >
-            {selectItems.fields['company'].map(({ value, label }) => {
-                return (
-                    <option key={value} value={value}>
-                        {label}
-                    </option>
-                );
-            })}
-        </select>
-        <br />
-        <br />
-        <select
-            id="size2"
-            name="size2"
-            onChange={(e) => handleChange("size2", e.target.value)}
-        >
-            {selectItems.fields[values.company].map(({ value, selected }) => {
-                return (
-                    <option key={value} value={value}>
-                        {value}
-                    </option>
-                );
-            })}
-        </select>
-        </div>
-        </ToggleVisibility>
-        </div>
+
+      <span style={{color:'red'}}>DODAJ KOMPUTER</span>
+      <ToggleVisibility>
+        <AddComputer onSubmit={addComputer} />
+      </ToggleVisibility>
+
+      <span style={{color:'red'}}>WYSZUKAJ KOMPUTER</span>
+      <ToggleVisibility>
+      <QueryComputer submitLabel="Wyszukaj" onSubmit={queryComputers} />
+          {queryComputer.length === 0 ?
+               ''
+               :
+               <ul>
+                   {queryComputer.map(computer => (
+                      <li key={computer.id}>
+                          <Computer computer={computer} onUpdate={updateComputer} onDelete={deleteComputer} />
+                      </li>
+                   ))}
+               </ul>
+          }
+      </ToggleVisibility>
+
+      <span style={{color:'red'}}>Lista modeli komputerów w przedsiębiorstwie</span>
+      <ToggleVisibility>
+      <div>
+      <select
+          id="company"
+          name="company"
+          onChange={(e) => handleChange("company", e.target.value)}
+      >
+          {companies.length === 0 ?
+              'Ładuje się ...'
+              :
+                  companies.map(({value, label}) => {
+                      return (
+                          <option key={value} value={value}>
+                              {label}
+                          </option>
+                      );
+                  })
+          }
+      </select>
+      <br />
+      <br />
+      <select
+          id="models"
+          name="models"
+          onChange={(e) => handleChange("models", e.target.value)}
+      >
+          {selectItems.fields[values.company].map(({ value, selected }) => {
+               return (
+                  <option key={value} value={value}>
+                      {value}
+                  </option>
+               );
+          })}
+      </select>
+      </div>
+      </ToggleVisibility>
+
     </>
   );
 }
